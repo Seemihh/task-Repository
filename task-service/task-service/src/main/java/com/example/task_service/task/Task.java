a345aaa0f87d6f529a2cdd09a69d7bd5ad5f3b40
Seemihh <semih.ozkara60@gmail.com>
2024-07-29 10:15:17 +0300

    Database Settled

--- task-service/task-service/src/main/java/com/example/task_service/task/Task.java
+++ task-service/task-service/src/main/java/com/example/task_service/task/Task.java
@@ -1,17 +1,28 @@
 package com.example.task_service.task;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 @Getter
 @Setter
+@Table(name = "Task")
+@Entity
 public class Task {
-
+    @Id
+    @Column(name = "ID")
     private long id;
+    @Column(name = "Title")
     private String title;
+    @Column(name = "StartDate")
     private LocalDateTime startDate;
+    @Column(name = "EndDate")
     private LocalDateTime endDate;
+    @Column(name = "FlightID")
     private long flightId;
 
 }
